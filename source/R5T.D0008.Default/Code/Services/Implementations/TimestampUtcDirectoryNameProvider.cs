9f0434508aeccd3dddc68ad90cbf12bdf1982835
MinexAutomation <minex.automation@gmail.com>
2020-05-15 12:11:06 -0700

    Added R5T.D0008.Default library. Added TimestampUtcDirectoryNameProvider implementation.

---
+++ source/R5T.D0008.Default/Code/Services/Implementations/TimestampUtcDirectoryNameProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.Magyar.Extensions;
+
+
+namespace R5T.D0008.Default
+{
+    public class TimestampUtcDirectoryNameProvider : ITimestampUtcDirectoryNameProvider
+    {
+        public Task<string> GetTimestampUtcDirectoryName(DateTime timestampUtc)
+        {
+            var timestampUtcDirectoryName = timestampUtc.ToYYYYMMDD_HHMMSS();
+
+            return Task.FromResult(timestampUtcDirectoryName);
+        }
+    }
+}
