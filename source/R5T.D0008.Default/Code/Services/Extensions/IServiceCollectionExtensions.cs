75129e66f8f71a1d10e9cfb2b54376b80803bb7f
MinexAutomation <minex.automation@gmail.com>
2020-05-15 12:26:57 -0700

    Added R5T.D0008.Standard library.

---
+++ source/R5T.D0008.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.D0008.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="TimestampUtcDirectoryNameProvider"/> implementation of <see cref="ITimestampUtcDirectoryNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultTimestampUtcDirectoryNameProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<ITimestampUtcDirectoryNameProvider, TimestampUtcDirectoryNameProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="TimestampUtcDirectoryNameProvider"/> implementation of <see cref="ITimestampUtcDirectoryNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ITimestampUtcDirectoryNameProvider> AddDefaultTimestampUtcDirectoryNameProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<ITimestampUtcDirectoryNameProvider>.New(() => services.AddDefaultTimestampUtcDirectoryNameProvider());
+            return serviceAction;
+        }
+    }
+}
---
+++ source/R5T.D0008.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.D0008.Default;
+
+
+namespace R5T.D0008.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="ITimestampUtcDirectoryNameProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddTimestampUtcDirectoryNameProvider(this IServiceCollection services)
+        {
+            services.AddDefaultTimestampUtcDirectoryNameProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ITimestampUtcDirectoryNameProvider"/> service.
+        /// </summary>
+        public static ServiceAction<ITimestampUtcDirectoryNameProvider> AddTimestampUtcDirectoryNameProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<ITimestampUtcDirectoryNameProvider>.New(() => services.AddTimestampUtcDirectoryNameProvider());
+            return serviceAction;
+        }
+    }
+}
